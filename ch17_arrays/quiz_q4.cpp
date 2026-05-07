05879e919add44797c7d7260409364ccbcb079fb
Hamzah Hussain <hamzah@DESKTOP-8RJV4SG.localdomain>
2026-05-07 15:37:41 +0100

    push to continue working on macbook, ankle injury :(

--- ch17_arrays/quiz_q4.cpp
+++ ch17_arrays/quiz_q4.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 
 #include "deck.hpp"
 #include "settings.hpp"
@@ -33,11 +34,43 @@ bool hasPlayerWon(Player& dealer, Player& player) {
     return false;
 }
 
+bool hasGoneBust(Player& player) {
+    if (player.score > settings::blackjack) {
+        return true;
+    }
+    return false;
+}
+
 void handleDealersTurn(Player& dealer, Deck& deck) {
     do {
         auto& card{dealCard(dealer, deck)};
         std::cout << "The dealer flips a " << card << ". They now have: " << dealer.score << "\n";
-    } while (dealer.score < settings::dealerLimit);  // handle dealer going bust!
+    } while ((dealer.score < settings::dealerLimit) &&
+             !hasGoneBust(dealer));  // handle dealer going bust!
+    if (hasGoneBust(dealer)) {
+        std::cout << "The dealer went bust!\n";
+    }
+}
+
+void handlePlayerTurn(Player& player, Deck& deck, char input) {
+    auto& card{dealCard(player, deck)};
+    std::cout << "You were dealt " << card << ". You now have: " << player.score << "\n";
+}
+
+char handlePlayerInput() {
+    char input{};
+    while (true) {
+        std::cout << "(h) to hit, or (s) to stand: ";
+        std::cin >> input;
+        if (std::cin.fail() || std::cin.peek() != '\n' || (input != 'h' || input != 's')) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        break;
+    }
+    return input;
 }
 
 void playBlackjack(Player& dealer, Player& player, Deck& deck) {
@@ -46,12 +79,24 @@ void playBlackjack(Player& dealer, Player& player, Deck& deck) {
     dealInitialCards(dealer, player, deck);
     displayScores(dealer, player);
 
+    while (true) {
+        char input{handlePlayerInput()};
+        if (input == 's') {
+            break;
+        }
+        handlePlayerTurn(player, deck, input);
+        if (hasGoneBust(player)) {
+            std::cout << "You went bust!\n";
+            break;
+        }
+    }
+
     handleDealersTurn(dealer, deck);
 
     if (hasPlayerWon(dealer, player)) {
-        std::cout << "You win!";
+        std::cout << "You win!\n";
     } else {
-        std::cout << "You lose!";
+        std::cout << "You lose!\n";
     }
 }
 
@@ -61,3 +106,5 @@ int main() {
     Deck deck;
     playBlackjack(dealer, player, deck);
 }
+
+// this logic is all fucked up. need to think about this properly.
